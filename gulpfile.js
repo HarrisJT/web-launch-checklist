119fa67c69b1e028fba8abf87f9bf4e0c6afca30
Harris Thompson <hjthompson@live.com>
2017-04-11 18:33:14 -0500

    Update gulp and normalize

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const autoprefixer = require('gulp-autoprefixer');
 const csscomb = require('gulp-csscomb');
-const minify = require('gulp-minify-css');
-const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('js', function () {
   return gulp.src('_include/js/main.js')
@@ -21,20 +20,13 @@ gulp.task('css', function () {
       }))
       .pipe(csscomb())
       .pipe(concat('main.min.css'))
-      .pipe(minify())
+      .pipe(cleanCSS())
       .pipe(gulp.dest('_include/css'));
 
 });
 
-gulp.task('img', function () {
-  return gulp.src('_include/img/*.{png,gif,jpg}')
-      .pipe(imagemin())
-      .pipe(gulp.dest('_include/img'));
-});
-
-gulp.task('default', gulp.series('js', 'css', 'img', function (done) {
-  gulp.watch('_include/js/main.js').on('change', gulp.task('js'));
-  gulp.watch('_include/css/main.css').on('change', gulp.task('css'));
-  gulp.watch('_include/img/**/*.{png,gif,jpg}').on('add', gulp.task('img'));
+gulp.task('default', gulp.series('js', 'css', function (done) {
+  gulp.watch('_include/js/main.js', gulp.task('js'));
+  gulp.watch('_include/css/main.css', gulp.task('css'));
   done();
 }));
